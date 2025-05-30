2eb4dcbc97899d8b42a35eba46f30b6c1b719096
XF <1131680660@qq.com>
2025-05-30 18:20:44 +0800

    feat:装载玩家技能的初始化的命令

--- scriptFiles/game/MainStorage/code/server/CommandSystem/commands/MSkillCommands.lua
+++ scriptFiles/game/MainStorage/code/server/CommandSystem/commands/MSkillCommands.lua
@@ -10,48 +10,13 @@ local cloudDataMgr = require(MainStorage.code.server.MCloudDataMgr)  ---@type MC
 local SkillCommands = {}
 
 -- --装载配置的文件的技能
-function SkillCommands.UnlockSkill(params, player)
-    if params.category ~= "技能" then return false end
-    
-    local skillType = params.subcategory  -- 火系, 水系等
-    local skillId = tonumber(params.id)
-    
-    -- 检查技能是否存在
-    if not common_config.skill_def[skillId] then
-        gg.log("技能不存在: " .. skillId)
+function SkillCommands.LoadDefSkill(params, player)
+    local player = gg.getPlayerByUin(params.uin)
+    if not player then
+        gg.log("玩家不存在: " .. params.uin)
         return false
     end
-    
-    -- 寻找空闲的技能栏位
-    local skillSlot = nil
-    for i = 1, 6 do
-        if not player.dict_btn_skill[i] or player.dict_btn_skill[i] == 0 then
-            skillSlot = i
-            break
-        end
-    end
-    
-    if not skillSlot then
-        -- 技能栏已满
-        gg.network_channel:fireClient(player.uin, {
-            cmd = "cmd_client_show_msg",
-            txt = "技能栏已满，请先移除一个技能",
-            color = ColorQuad.New(255, 0, 0, 255)
-        })
-        return false
-    end
-    
-    -- 添加技能
-    player.dict_btn_skill[skillSlot] = skillId
-    player:saveSkillConfig()
-    
-    -- 通知客户端
-    gg.network_channel:fireClient(player.uin, {
-        cmd = "cmd_client_show_msg",
-        txt = "解锁技能: " .. common_config.skill_def[skillId].name,
-        color = ColorQuad.New(0, 255, 0, 255)
-    })
-    
+    player:LoadingConfSkills()
     return true
 end
 
